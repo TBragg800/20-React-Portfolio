e137957965fd757b9a8c1acf33491fd270b71a69
LM&T <tbbragg83@gmail.com>
2020-09-29 16:02:47 -0500

    commiting file structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./components/Header";
+import TableHeader from "./components/TableHeader";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TableHeader /> 
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?results=50";
+
+
+export default {
+  searchEmployees: function() {
+      return axios.get(BASEURL);
+  }
+};
