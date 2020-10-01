c0c1004639f218bbbe876d637b8c3dafdb618687
LM&T <tbbragg83@gmail.com>
2020-10-01 00:53:04 -0500

    home elements rendered

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,82 +2,94 @@ import React from "react";
 
 function NavTabs(props) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => props.handlePageChange("Home")}
-          className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => props.handlePageChange("Portfolio")}
-          className={props.currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => props.handlePageChange("Contact")}
-          className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => props.handlePageChange("Resume")}
-          className={props.currentPage === "Resume" ? "nav-link active" : "nav-link"}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
+    // <ul className="nav nav-tabs">
+    //   <li className="nav-item">
+    //     <a
+    //       href="#home"
+    //       onClick={() => props.handlePageChange("Home")}
+    //       className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
+    //     >
+    //       Home
+    //     </a>
+    //   </li>
+    //   <li className="nav-item">
+    //     <a
+    //       href="#portfolio"
+    //       onClick={() => props.handlePageChange("Portfolio")}
+    //       className={props.currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
+    //     >
+    //       Portfolio
+    //     </a>
+    //   </li>
+    //   <li className="nav-item">
+    //     <a
+    //       href="#contact"
+    //       onClick={() => props.handlePageChange("Contact")}
+    //       className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+    //     >
+    //       Contact
+    //     </a>
+    //   </li>
+    //   <li className="nav-item">
+    //     <a
+    //       href="#resume"
+    //       onClick={() => props.handlePageChange("Resume")}
+    //       className={props.currentPage === "Resume" ? "nav-link active" : "nav-link"}
+    //     >
+    //       Resume
+    //     </a>
+    //   </li>
+    // </ul>
 
-    // <header>
-    //   <nav className="navbar navbar-expand-lg mynavbar bg-transparent"
-    //     data-aos="fade-up"
-    //     data-aos-offset="0"
-    //     data-aos-delay="50"
-    //     data-aos-duration="1000"
-    //     data-aos-easing="ease-in-out"
-    //     data-aos-mirror="true"
-    //     data-aos-once="true"
-    //     data-aos-anchor-placement="top-center" >
-    //         <a className="navbar-brand" data-toggle="collapse" href="#collapseExample"><h1 className="display-4">Thomas</h1></a>
-    //         <a className="nav-link disabled collapse" id="collapseExample" href="#" tabindex="-1" aria-disabled="true"><h1 className="display-4 bg-transparent">Bragg</h1></a>
+    <header>
+      <nav className="navbar navbar-expand-lg mynavbar bg-transparent"
+        data-aos="fade-up"
+        data-aos-offset="0"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="true"
+        data-aos-anchor-placement="top-center" >
+            <a className="navbar-brand" data-toggle="collapse" href="#collapseExample"><h1 className="display-4">Thomas</h1></a>
+            <a className="nav-link disabled collapse" id="collapseExample" href="#" tabindex="-1" aria-disabled="true"><h1 className="display-4 bg-transparent">Bragg</h1></a>
 
-    //         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    //           <span className="navbar-toggler-icon"></span>
-    //         </button>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-    //         <section className="collapse navbar-collapse visible" id="navbarNav">
-    //           <ul className="navbar-nav">
-    //             <li className="nav-item">
-    //                 <a className="nav-link navFont" href="index.html">HOME</a>
-    //             </li>
-    //             <li className="nav-item">
-    //                 <a className="nav-link navFont" href="portfolio.html">PORTFOLIO</a>
-    //             </li>
-    //             <li className="nav-item">
-    //                 <a className="nav-link navFont" href="contact.html">CONTACT</a>
-    //             </li>
-    //             <li className="nav-item">
-    //                 <a className="nav-link navFont" href="resume.html">RESUME</a>
-    //             </li>
-    //           </ul>
-    //         </section>
+            <section className="collapse navbar-collapse visible" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link navFont" 
+                    className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
+                    onClick={() => props.handlePageChange("Home")}
+                    href="#home">HOME</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link navFont" 
+                    className={props.currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
+                    onClick={() => props.handlePageChange("Portfolio")}
+                    href="#portfolio">PORTFOLIO</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link navFont"
+                    className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+                    onClick={() => props.handlePageChange("Contact")} 
+                    href="#contact">CONTACT</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link navFont"
+                    className={props.currentPage === "Resume" ? "nav-link active" : "nav-link"}
+                    onClick={() => props.handlePageChange("Resume")} 
+                    href="#resume">RESUME</a>
+                </li>
+              </ul>
+            </section>
           
-    //   </nav>
+      </nav>
 
-    // </header>
+    </header>
 
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,19 +1,72 @@
 import React from "react";
 
 const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-      varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-      Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-      imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
-      in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare elit.
-      Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-      consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-      fames ac ante ipsum primis in faucibus.
-    </p>
-  </div>
+  <main>    
+ {/* Container for main content  */}
+        <section className="container bg-transparent rounded mb-5">
+{/* H1 element to head page  */}
+            <div className="row h1bg"
+            data-aos="fade-down"
+            data-aos-offset="0"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="true"
+            data-aos-anchor-placement="top-center">
+                <div className="col row justify-content-center">
+                    <h1>About Me</h1>
+                </div>
+            </div>
+{/* Div containing figure, carousel and description  */}
+            <div className="row m-5"
+            data-aos="zoom-in"
+            data-aos-offset="-150"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="true"
+            data-aos-anchor-placement="top-center">
+                <figure className="col-sm-4 mb-3">
+                    <img src="redshirt2.JPG" className="mycol rounded-pill shadow-lg mycenter" alt="Thomas in red shirt" />
+                </figure>
+
+                <article className="col-lg-8 float-right shadow-lg p-3 mb-5 bg-white rounded">
+{/* Div for carousel */}
+                    <div className="row m-3 justify-content-center">  
+                        <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+                            <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <h4 className="myp1 shadow-lg">I am a Musician...</h4>
+                            </div>
+                            <div className="carousel-item">
+                                <h4 className="myp1 shadow-lg">I am a Bartender...</h4>
+                            </div>
+                            <div className="carousel-item">
+                                <h4 className="myp1 shadow-lg">& a Full Stack Developer!</h4>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+{/* Div for description */}
+                    <div className="row m-3"> 
+                        <p className="myp2">Hi, Thomas here, and I am FULL STACK DEVELOPER!! I live in Nashville, 
+                            Tenneessee and currently attend The Vanderbilt University Full Stack Flex Program. I'm new to 
+                            the web design community but feel like I am in a great time and place for evolving and challenging
+                            myself. I love the logic and structure of coding and strive to utlize HTML, CSS and Java Scripts 
+                            to make elegant and efficient code. I love traveling, the arts, comedy, I'm very easy to get into a 
+                            conversation about dogs or music and I will always take the opportunity to learn something new.
+                        </p>
+                    </div>
+
+                </article>
+            
+            </div>
+        
+        </section>
+    
+    </main>
 );
 
 export default Home;
