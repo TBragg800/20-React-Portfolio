54449126a1bc5be461d2c06d33f471ad97275f80
LM&T <tbbragg83@gmail.com>
2020-10-04 15:11:02 -0500

    redploy/build files

---
+++ build/precache-manifest.60a080e2a8c51545afdfb52625cbc224.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff98925394b02dee7dd8e0208986a229",
+    "url": "/20-React-Portfolio/index.html"
+  },
+  {
+    "revision": "d69862e5b354ca04b69a",
+    "url": "/20-React-Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "89be12b53f56732c270c",
+    "url": "/20-React-Portfolio/static/css/main.6249eb12.chunk.css"
+  },
+  {
+    "revision": "d69862e5b354ca04b69a",
+    "url": "/20-React-Portfolio/static/js/2.70447a83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/20-React-Portfolio/static/js/2.70447a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89be12b53f56732c270c",
+    "url": "/20-React-Portfolio/static/js/main.363a7da7.chunk.js"
+  },
+  {
+    "revision": "0c35aa27913b3b1d5002",
+    "url": "/20-React-Portfolio/static/js/runtime-main.3fe209d1.js"
+  },
+  {
+    "revision": "fa805fd0a9aae67583247a34d0639d16",
+    "url": "/20-React-Portfolio/static/media/constellations.fa805fd0.gif"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/20-React-Portfolio/precache-manifest.0dcf504b3adacb8a43a17a58d446d03a.js"
+  "/20-React-Portfolio/precache-manifest.60a080e2a8c51545afdfb52625cbc224.js"
 );
 
 self.addEventListener('message', (event) => {
