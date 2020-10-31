6a6ae80949335697c70c79af764c4f48a1c96540
LM&T <tbbragg83@gmail.com>
2020-10-31 13:25:37 -0500

    changed contact page to a class component/onChange handler updating state

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Contact = () => (
-  <main> 
- {/* Container for main content    */}
-      <section className="container myborder bg-transparent rounded mb-5 shadow-lg">
- {/* H1 element to head page  */}
-        <section className="row h1bg"
-        data-aos="fade-down"
-        data-aos-offset="0"
-        data-aos-delay="50"
-        data-aos-duration="1000"
-        data-aos-easing="ease-in-out"
-        data-aos-mirror="true"
-        data-aos-once="true"
-        data-aos-anchor-placement="top-center">
-              <div className="col row justify-content-center">
-                  <h1>Contact</h1>
-              </div>
-          </section>
- {/* Form section containing contact forms */}
+class Contact extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    return (
+      <main> 
+    {/* Container for main content    */}
+        <section className="container myborder bg-transparent rounded mb-5 shadow-lg">
+    {/* H1 element to head page  */}
+          <section className="row h1bg"
+          data-aos="fade-down"
+          data-aos-offset="0"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-once="true"
+          data-aos-anchor-placement="top-center">
+                <div className="col row justify-content-center">
+                    <h1>Contact</h1>
+                </div>
+            </section>
+    {/* Form section containing contact forms */}
           <section className="row m-5">
             <div className="col-lg-12 float-right shadow-lg p-3 mb-5 bg-transparent rounded"
             data-aos="zoom-in"
@@ -32,28 +49,42 @@ const Contact = () => (
               <form>
                 <div className="form-row">
                   <div className="col">
-                    <label for="validationCustom01" className="myp3">First name</label>
-                    <input type="text" className="form-control" id="validationCustom01" />
-                  </div>
-                  <div className="col">
-                    <label for="validationCustom02" className="myp3">Last name</label>
-                    <input type="text" className="form-control" id="validationCustom02" />
+                    <label for="validationCustom01" className="myp3">Name</label>
+                    <input 
+                      type="text"
+                      name="name"
+                      className="form-control" 
+                      id="validationCustom01"
+                      onChange={this.handleChange} />
                   </div>
                 </div>
                 <div className="form-group mt-2">
                   <label for="exampleInputEmail1" id="emailHelp" className="myp3">Email address</label>
-                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                  <input 
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleChange} />
                 </div>
                 <div className="form-group">
                   <label for="exampleFormControlTextarea1" className="myp3">Message</label>
-                  <textarea className="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+                  <textarea
+                  name="message"
+                  className="form-control" 
+                  id="exampleFormControlTextarea1" 
+                  rows="10"
+                  onChange={this.handleChange} />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
               </form>
             </div>
           </section>    
-      </section>
-    </main>
-);
+        </section>
+      </main>
+    );
+  }
+};
 
 export default Contact;
